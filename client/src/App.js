6a76d58861d9bcd459583ea890571c2b0dd75039
Imperial Design <amsanchez@knox.edu>
2021-01-14 23:48:36 -0600

    add Nav Bar and Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Home from './components/Home';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <div>
+        <Nav />
       <main>
           <Home />        
       </main>
+      <Footer />
     </div>
   );
 }
