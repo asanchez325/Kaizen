c56a8fdd3a18a0a0f9b90d9c7665a4305e84572e
Jon Sanchez <jonathan.sanchez@windstream.net>
2021-01-20 10:24:50 -0700

    Heroku Push

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.use('/posts', postRoutes);
 const MONGODB_URI = 'mongodb+srv://JCS_Develop:JCS_Develop12@cluster0.hwkts.mongodb.net/Memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname,  'client', 'build', 'index.html'));
+  })
+}
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
