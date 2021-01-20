86c5c4021eaffba978a1af87f2e09e5a6d123a35
Imperial Design <amsanchez@knox.edu>
2021-01-19 21:51:13 -0600

    add kaizen mongodb

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const MONGODB_URI = 'mongodb+srv://JCS_Develop:JCS_Develop12@cluster0.hwkts.mongodb.net/Memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/kaizen"
+
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
