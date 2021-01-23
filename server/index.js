a0f82e35f9e1c461af5b651618eb13c7d663d8a4
Imperial Design <amsanchez@knox.edu>
2021-01-22 22:52:17 -0600

    try new index

--- server/index.js
+++ server/index.js
@@ -1,32 +1,37 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const mongoose = require ('mongoose');
-const cors = require ('cors');
-const path = require ('path');
+  
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-import postRoutes from './routes/posts.js';
+const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+// use body parser to get data from POST requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/posts', postRoutes);
+// Use API routes from the api folder
+const apis = require("./api");
+app.use("/api", apis);
 
-const MONGODB_URI = 'mongodb+srv://asanchez325:1234@cluster0.7bkbq.mongodb.net/<dbname>?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 3030;
+// If in production, then use static frontend build files.
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('./client/build'));
-
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname,  'client', 'build', 'index.html'));
-  })
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.js'));
+    });
 }
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+// Connect to Mongo
+ mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
 
-mongoose.set('useFindAndModify', false);
+app.listen(port, () => console.log(`Listening on port ${port}`));
